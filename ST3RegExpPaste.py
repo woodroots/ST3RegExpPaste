a10b5295d4be9f9f78e150a149e7227e54fadfea
Takahiro Morimoto <morimoto@wood-roots.com>
2014-03-25 21:21:09 +0900

    1st commit

---
+++ ST3RegExpPaste.py
@@ -0,0 +1,18 @@
+import sublime
+import sublime_plugin
+import re
+
+#TXS
+class myfuncRegCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+
+        for region in self.view.sel():
+            txt = sublime.get_clipboard()
+            esc_txt = re.sub(r'([\.\\\+\*\?\[\^\]\$\(\)\{\}\=\!\|\:])', r'\\\1', txt)
+
+            #クオート内を何でもありにする
+            #いらなかったらコメントアウトしてください
+            esc_txt = re.sub(r'".+?"', '"(.+?)"', esc_txt)
+
+            sublime.set_clipboard(esc_txt)
+            self.view.window().run_command("paste")
